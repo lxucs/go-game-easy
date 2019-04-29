67569845e2ce54b3987cec7f54e1dbaf4e58bf03
lxucs <leonxu624@gmail.com>
2019-04-29 02:33:20 -0400

    Refined rl training

--- agent/rl_agent.py
+++ agent/rl_agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from game.go import Board
 from game.go import opponent_color
 import random
+from statistics import mean
 
 
 class RlAgent(Agent):
@@ -14,9 +15,6 @@ class RlAgent(Agent):
 
     def get_action(self, board):
         raise NotImplementedError
-
-    def __str__(self):
-        return self.__class__.__name__ + str(hash(self))
 
 
 class ApproxQAgent(RlAgent):
@@ -36,11 +34,15 @@ class ApproxQAgent(RlAgent):
     def save(self):
         """Save the weight vector."""
         if self.w is not None:
-            np.save(self.__class__.__name__, self.w)
+            path_file = str(self) + '.npy'
+            np.save(path_file, self.w)
+            print('Saved weights to ' + path_file)
 
     def load(self):
         """Load the weight vector."""
-        self.w = np.load(self.__class__.__name__ + '.npy')
+        path_file = str(self) + '.npy'
+        self.w = np.load(path_file)
+        print('Loaded weights from ' + path_file)
 
     def train(self, epochs, lr, discount, exploration_rate):
         """
@@ -57,10 +59,20 @@ class ApproxQAgent(RlAgent):
         num_feats = self.rl_env.get_num_feats()
         self.w = np.random.random(num_feats)
 
+        exploration_rate_decay = 0.9
+        print('Start training ' + str(self))
         for epoch in range(epochs):
-            self._train_one_epoch(lr, discount, exploration_rate)
+            diff_mean = self._train_one_epoch(lr, discount, exploration_rate)
+            # Decay exploration_rate
+            if epoch % 100 == 99:
+                exploration_rate *= exploration_rate_decay
+                print('Decay exploration_rate to %f' % exploration_rate)
+            # Echo performance
+            if epoch % 5 == 4:
+                print('Epoch %d: mean difference %f' % (epoch, diff_mean))
 
     def _train_one_epoch(self, lr, discount, exploration_rate):
+        """Return the mean of difference during this epoch"""
         agent_oppo = RandomAgent(opponent_color(self.color))
         board = Board()
         first_move = (10, 10)
@@ -69,6 +81,7 @@ class ApproxQAgent(RlAgent):
         if board.next != self.color:
             board.put_stone(agent_oppo.get_action(board), check_legal=False)
 
+        diffs = []
         while board.winner is None:
             legal_actions = board.get_legal_actions()
 
@@ -97,9 +110,19 @@ class ApproxQAgent(RlAgent):
                 next_qs = [self._calc_q(board, action) for action in next_legal_actions]
                 reward_future = max(next_qs)
             difference = reward_now + discount * reward_future - q
+            diffs.append(difference)
 
             # Apply weight update
             self.w += (lr * difference * feats)
 
+        return mean(diffs)
+
     def _calc_q(self, board, action):
         return self.w.dot(self.rl_env.extract_features(board, action, self.color))
+
+
+if __name__ == '__main__':
+    # Train and save ApproxQAgent
+    approx_q_agent = ApproxQAgent('BLACK', RlEnv())
+    approx_q_agent.train(5000, 0.1, 0.8, 0.05)
+    approx_q_agent.save()
